beac8890b3376691d103322952b56d0b360c148e
arch0tic <arch0tic@protonmail.com>
2024-10-26 12:57:42 +0000

    A script for my darling. I love her soo much.

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+install_things(){
+	pacman -Syu i3 xorg xorg-init polybar rofi nitrogen rofi sddm dhcpcd dhcp grub efibootmgr dosfostools os-prober mtools networkmanager git terminator firefox wget vim sudo
+	systemctl enable dhcpcd
+	systemctl enable NetworkManager
+}
+install_themes(){
+	mkdir Desktop Documents Downloads Pictures Downloads/git-downloads Pictures/Wallpapers Pictures/Screenshots
+	sudo pacman -Syu plasma lxappearance gtk-engines
+	cp themes/i3/config ~/.config/i3/
+	cp -r themes/polybar ~/.config/
+	cp -r themes/rofi ~/.config/
+	sudo cp -r themes/candy-icons /usr/share/icons/
+	sudo cp -r themes/Sweet-cursors /usr/share/icons/
+	sudo cp -r themes/Sweet-Dark-v40 /usr/share/themes/
+	sudo cp -r themes/sweet-plasma6 /usr/share/sddm/themes/
+	git clone https://aur.archlinux.org/yay
+	cd yay 
+	makepkg -si
+	yay -S sublime-text-4
+}
+
+
+main(){
+	echo -e '''
+
+ █████╗ ██████╗  ██████╗██╗  ██╗███╗   ██╗███████╗████████╗
+██╔══██╗██╔══██╗██╔════╝██║  ██║████╗  ██║██╔════╝╚══██╔══╝
+███████║██████╔╝██║     ███████║██╔██╗ ██║█████╗     ██║   
+██╔══██║██╔══██╗██║     ██╔══██║██║╚██╗██║██╔══╝     ██║   
+██║  ██║██║  ██║╚██████╗██║  ██║██║ ╚████║███████╗   ██║   
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   
+                                                           
+
+	'''
+	echo -e "[ First option is for apps install in CHROOT ] "
+	echo -e "[ And the second option is for themeing only ] "
+	read -p "Insert option: " option
+	if [[ $option == "1" ]] then
+		install_things
+	elif [[ $option == "2" ]] then
+		install_themes
+	fi
+}
+
+main
